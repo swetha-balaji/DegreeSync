f2b8c0d32447560322e5d5cc5e679e507ab3f1fd
jayp-8 <104044092+jayp-8@users.noreply.github.com>
2024-09-20 13:04:30 -0400

    connect MongoDB atlas database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const expressLayouts = require("express-ejs-layouts");
-
+const User = require('./models/User');
 
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://mongodB:eqJtPNhRbA4asm8n@cluster0.skb4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 app.use(expressLayouts);
 app.set('layout', './layout/layout')
