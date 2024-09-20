5bdcbbe74f3009617d5422fdb6630d9ad23c645b
jayp-8 <104044092+jayp-8@users.noreply.github.com>
2024-09-20 13:50:25 -0400

    Add files via upload

---
+++ models/Student.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    major: {
+        type: String,
+        required: true
+    },
+    classification: {
+        type: String,
+        required: true
+    },
+    studentid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    student_name: {
+        type: String,
+        required: true
+    },
+    concentration: {
+        type: String,
+        required: true
+    },
+    advisor: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Student', studentSchema);
